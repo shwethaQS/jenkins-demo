3f10679d4246f4b8b973ef5a1cd0fba7de9144d4
shwethaQS <55647175+shwethaQS@users.noreply.github.com>
2021-10-18 21:11:26 +0200

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,3 +20,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('theme-park-job-aws') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/shwethaQS/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-aws')
+                }
+            }
+        }
+    }
+}
